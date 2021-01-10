a72e934fa7bb7236cdda7c87299bde51f4176def
Adam Wathan <adam.wathan@gmail.com>
2021-01-10 16:13:55 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      ringWidth: ['focus-visible'],
+      ringColor: ['focus-visible'],
+      ringOffsetWidth: ['focus-visible'],
+      ringOffsetColor: ['focus-visible'],
+    },
+  },
+  plugins: [],
+}
